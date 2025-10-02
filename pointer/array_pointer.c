c8dce47aba38e07030193c74b294d01974c009f4
aasthapro <iamaastha2004@gmail.com>
2025-10-02 14:34:54 +0530

    Update array_pointer.c

--- pointer/array_pointer.c
+++ pointer/array_pointer.c
@@ -1,13 +1,22 @@
 /* A program to learn pointer basics */
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int array[5];
 
     printf("array=    %p\n", array);
+
+    printf("\n");
+    printf("array + 0=%p\n", array + 0);
     printf("&array[0]=%p\n", &array[0]);
+
+    printf("\n");
+    printf("array + 1=%p\n", array + 1);
     printf("&array[1]=%p\n", &array[1]);
+
+    printf("\n");
+    printf("array + 2=%p\n", array + 2);
     printf("&array[2]=%p\n", &array[2]);
 
-
     return 0;
- }
+}
