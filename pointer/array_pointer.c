4106bc442b80c0794a515e60c9a6900922be88d7
aasthapro <iamaastha2004@gmail.com>
2025-10-02 14:21:21 +0530

    adding array_pointer file

---
+++ pointer/array_pointer.c
@@ -0,0 +1,13 @@
+/* A program to learn pointer basics */
+#include<stdio.h>
+int main(){
+    int array[5];
+
+    printf("array=    %p\n", array);
+    printf("&array[0]=%p\n", &array[0]);
+    printf("&array[1]=%p\n", &array[1]);
+    printf("&array[2]=%p\n", &array[2]);
+
+
+    return 0;
+ }
